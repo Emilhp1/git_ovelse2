96c23f339e1e79207f3dd787c3584a63a642649f
Emilhp1 <124664688+Emilhp1@users.noreply.github.com>
2023-08-24 10:48:43 +0200

    js

---
+++ script.js
@@ -0,0 +1,17 @@
+const afsnit = document.querySelector("p");
+console.log(afsnit.textContent);
+afsnit.textContent = "Nu er det torsdag";
+
+const afsnit1 = document.querySelector("h1");
+const afsnit2 = document.querySelector("h2");
+const afsnit3 = document.querySelector("h3");
+const afsnit4 = document.querySelector(".p2");
+
+afsnit1.textContent = "Nu er det torsdag1";
+console.log(afsnit1.textContent);
+
+afsnit2.textContent = "Nu er det torsdag2";
+console.log(afsnit2.textContent);
+
+afsnit4.textContent = "Nu er det torsdag3";
+console.log(afsnit4.textContent);
